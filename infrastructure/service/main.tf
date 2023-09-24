6337df6001e78b7dc074050b213d4593f4465f06
kenko <kyoshitomi.cursor@gmail.com>
2023-09-24 18:43:50 +0900

    Create the execution role

--- infrastructure/service/main.tf
+++ infrastructure/service/main.tf
@@ -4,4 +4,15 @@ module "lambda" {
   source  = "../modules/lambda"
   commons = local.commons
   name    = "process"
+  policy_statements = [
+    {
+      "Effect" : "Allow",
+      "Action" : [
+        "sqs:ReceiveMessage",
+        "sqs:DeleteMessage",
+        "sqs:GetQueueAttributes",
+      ],
+      "Resource" : "*"
+    }
+  ]
 }
